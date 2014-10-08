f72ba273344064a7f40a8dc8cfa27a2f721931e4
arnpi <arnpi@gmx.com>
2014-10-08 09:03:39 +0200

    reorder route file

--- pynews/__init__.py
+++ pynews/__init__.py
@@ -18,14 +18,14 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_static_view('fonts', 'fonts', cache_max_age=3600)
+    config.add_static_view('fonts', 'static', cache_max_age=3600)
 
 # INDEX HTML
     config.add_route('index', '/')
 
 # ADMIN
-    config.add_route('users_list', '/users/list')
     config.add_route('users_add', '/users/add/{username}/{password}')
+    config.add_route('users_list', '/users/list')
     config.add_route('users_delete', '/users/delete/{username}/{password}')
 
 # LOGIN
@@ -33,34 +33,32 @@ def main(global_config, **settings):
     config.add_route('logout', '/logout')
     config.add_route('status_login', '/status_login')
 
-# LIST
+# CATEGORIES
+    config.add_route('categories_add', '/categories/add/{name}')
+    config.add_route('update_category', '/category/update/{category}/{name}')
+    config.add_route('categories_delete', '/categories/delete/{id}')
     config.add_route('categories_list', '/categories/list')
+
+# FLUXES
+    config.add_route('fluxes_add', '/fluxes/add/{title}/{text}/{category}')
+    config.add_route('view_feed', '/feed/{feed}')
+    config.add_route('update_rss_url', '/rss/update/url/{id_rss}/{url}')
+    config.add_route('update_rss_category', '/rss/update/category/{category}/{id}')
+    config.add_route('fluxes_delete', '/fluxes/delete/{id}')
     config.add_route('fluxes_list', '/fluxes/list')
     config.add_route('fluxes_list_by_category', '/fluxes_by_category/{category}')
-    config.add_route('notes_list', '/notes/list')
-    config.add_route('bookmarks_list', '/bookmarks/list')
 
-# Add
-    config.add_route('categories_add', '/categories/add/{name}')
-    config.add_route('fluxes_add', '/fluxes/add/{title}/{text}/{category}')
+# NOTES
     config.add_route('notes_add', '/notes/add/{title}/{text}')
-    config.add_route('bookmarks_add', '/bookmarks/add/{title}/{text}')
-
-# Delete
-    config.add_route('categories_delete', '/categories/delete/{id}')
-    config.add_route('fluxes_delete', '/fluxes/delete/{id}')
+    config.add_route('update_note', '/note/update/{note}/{text}')
     config.add_route('notes_delete', '/notes/delete/{id}')
-    config.add_route('bookmarks_delete', '/bookmarks/delete/{id}')
-
-# VIEW FEED
-    config.add_route('view_feed', '/feed/{feed}')
+    config.add_route('notes_list', '/notes/list')
     config.add_route('view_note', '/note/{note}')
 
-# UPDATE
-    config.add_route('update_note', '/note/update/{note}/{text}')
-    config.add_route('update_category', '/category/update/{category}/{name}')
-    config.add_route('update_rss_category', '/rss/update/category/{category}/{id}')
-    config.add_route('update_rss_url', '/rss/update/url/{id_rss}/{url}')
+# BOOKMARKS
+    config.add_route('bookmarks_add', '/bookmarks/add/{title}/{text}')
+    config.add_route('bookmarks_delete', '/bookmarks/delete/{id}')
+    config.add_route('bookmarks_list', '/bookmarks/list')
 
 # TWITTER
     config.add_route('register_api_twitter', '/register_api_twitter/{twitter_api_key}/{twitter_api_secret}')
